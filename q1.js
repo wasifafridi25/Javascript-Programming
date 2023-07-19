178fedc16320ae480f376da634c5d6d02d6ac703
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2023-07-19 01:28:11 -0700

    Done with 2 questions

---
+++ q1.js
@@ -0,0 +1,16 @@
+function showRating(rating){
+    let ratings = ""
+    for (let i = 0; i < Math.floor(rating);i++){
+        ratings += "*"
+        if (i != Math.floor(rating) - 1){
+            ratings += " "
+        }
+    }
+    if (!Number.isInteger(rating)){
+        ratings += " ."
+    }
+    return ratings
+    
+}
+
+console.log(showRating(5.5))
---
+++ q2.js
@@ -0,0 +1,6 @@
+function sortNumbers(nums){
+    return nums.sort((a, b) => a - b)
+}
+
+console.log(sortNumbers([11, 14, 9, 16, 22]))
+
